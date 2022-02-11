3ee31a100fe9633cb8cad7f0c5f5c8b2091ddb06
milcab <milcarodriguez23@gmail.com>
2022-02-11 12:25:21 -0500

    adding the challenge 1 (username and pic)

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import GitHubCard from './components/GitHubCard';
 
 function App() {
+  const users = [{
+    name: 'milcab23.js',
+    imageURL: 'https://avatar-endpoint.herokuapp.com/api/',
+  },
+  {
+    name: 'james.js',
+    imageURL: 'https://avatar-endpoint.herokuapp.com/api/',
+  },
+  {
+    name: 'joe.js',
+    imageURL: 'https://avatar-endpoint.herokuapp.com/api/',
+  },
+  {
+    name: 'mae.js',
+    imageURL: 'https://avatar-endpoint.herokuapp.com/api/',
+  }];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {users.map((user, i) => <GitHubCard key={i} {...user} index={i} />)}
     </div>
   );
+
 }
 
 export default App;
---
+++ src/components/GitHubCard.js
@@ -0,0 +1,19 @@
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+
+
+export default function GitHubCard({ name, imageURL, description, index }) {
+    return (
+        <Card style={{ width: '18rem' }} >
+            <Card.Img variant="top" src={`${imageURL}?index=${index}`} />
+            <Card.Body>
+                <Card.Title>{name}</Card.Title>
+                <Card.Text>
+                    {description}
+                </Card.Text>
+                <Button variant="primary">Go somewhere</Button>
+            </Card.Body>
+        </ Card >
+
+    );
+
