8c39004ff62ed7742ae9be9904cddc616edc453e
milcab <milcarodriguez23@gmail.com>
2022-02-10 20:17:12 -0500

    making the test works

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,8 @@
+import renderer from 'react-test-renderer'
+import App from './App'
+
 test('renders a snapshot', () => {
   const tree = renderer.create(<App />).toJSON()
+  console.log(tree)
   expect(tree).toMatchSnapshot()
 })
-
